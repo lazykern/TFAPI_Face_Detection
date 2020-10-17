e761e900dcd6e6b0555c6039a4a807f94b4e279b
Phusit Somboonyingsuk <56222561+lLoLii@users.noreply.github.com>
2020-10-17 11:02:13 +0700

    Update

--- gen_tfrecords.py
+++ gen_tfrecords.py
@@ -17,8 +17,6 @@ import io
 import pandas as pd
 import tensorflow as tf
 
-sys.path.append('/C:/Users/Admin/Desktop/models/research')
-
 from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
